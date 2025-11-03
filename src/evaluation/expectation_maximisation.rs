df6cc4bed599193dc02b1c325b38298e42bdd06c
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-11-03 11:38:57 +0000

    created enum for models

--- src/evaluation/expectation_maximisation.rs
+++ src/evaluation/expectation_maximisation.rs
@@ -1,6 +1,7 @@
 use crate::evaluation::em_result::EmResult;
+use crate::models::models::Models;
 
-pub async fn expectation_maximisation() -> EmResult{
+pub async fn expectation_maximisation(model: Models) -> EmResult{
     EmResult {
         guess: 0.0,
         transition: 0.0,
