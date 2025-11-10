5d1e7f8da78769bdbebd419122977f7ab2cfb4ee
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-11-10 14:47:19 +0000

    benchmark hmm

--- src/evaluation/performance/performance_benchmark.rs
+++ src/evaluation/performance/performance_benchmark.rs
@@ -5,20 +5,29 @@ use csv::ReaderBuilder;
 
 use crate::{
     evaluation::em_algorithm::{em_result::EmResult, formatted_record::FormattedRecord},
+    models::hidden_markov_model,
+    models::knowledge_tracing_model,
     models::models::Models,
 };
 
-#[derive(Debug)]
-struct User {
-    user_id: usize,
-    skills: HashMap<usize, f64>,
+
+
+async fn benchmark_hmm(users: &mut HashMap<u32, HashMap<u32, f64>>, records: &Vec<FormattedRecord>, transition: f64) {
+    println!("Starting HMM benchmark on {} records", records.len());
+    let now = Instant::now();
+    for record in records {
+        if let Some(skill_map) = users.get_mut(&record.user_id) {
+            if let Some(prob) = skill_map.get_mut(&record.skill_id) {
+                let new_prob = hidden_markov_model::calculate_mastery(*prob, transition).await;
+                *prob = new_prob;
+            }
+        }
+    }
+    let elapsed = now.elapsed();
+    println!("Finished HMM benchmark in {:?}", elapsed)
 }
 
-fn benchmark_hmm(students: &HashMap<usize, User>, records: &Vec<FormattedRecord>) {
-    
-}
-
-fn benchmark_ktm(students: &HashMap<usize, User>, records: &Vec<FormattedRecord>) {
+async fn benchmark_ktm(users: &HashMap<u32, HashMap<u32, f64>>, records: &Vec<FormattedRecord>) {
     
 }
 
@@ -40,26 +49,22 @@ pub async fn benchmark_model_performance(model: Models, initial_parameters: EmRe
         user_entry.entry(record.skill_id as usize)
             .or_insert(initial_parameters.initial); 
     }
-    let mut users: HashMap<usize, User> = HashMap::new();
+    let mut users: HashMap<u32, HashMap<u32, f64>> = HashMap::new();
 
     for record in &records {
-        let user = users
-            .entry(record.user_id as usize)
-            .or_insert_with(|| User {
-                user_id: record.user_id as usize,
-                skills: HashMap::new(),
-            });
-
-        user.skills
-            .entry(record.skill_id as usize)
+        let skill_map = users
+            .entry(record.user_id)
+            .or_insert_with(HashMap::new);
+        skill_map
+            .entry(record.skill_id)
             .or_insert(initial_parameters.initial);
     }
 
     println!("Initialized {} students with skill maps.", users.len());
 
     match model {
-        Models::HiddenMarkovModel => benchmark_hmm(&users, &records),
-        Models::KnowledgeTracingModel => benchmark_ktm(&users, &records),
+        Models::HiddenMarkovModel => benchmark_hmm(&mut users, &records, initial_parameters.transition).await,
+        Models::KnowledgeTracingModel => benchmark_ktm(&mut users, &records).await,
         _ => println!("Unsupported model type for benchmarking."),
     }
 
