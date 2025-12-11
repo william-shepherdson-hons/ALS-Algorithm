a56920a14e041ecb95f360258c91574e5c934e52
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-12-11 23:25:40 +0000

    new ide does fotmatting?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use als_algorithm::{evaluation::{em_algorithm::{em_result::EmResult, expectation_maximisation::expectation_maximisation}}, models::models::Models};
-
+use als_algorithm::{
+    evaluation::em_algorithm::{
+        em_result::EmResult, expectation_maximisation::expectation_maximisation,
+    },
+    models::models::Models,
+};
 
 #[tokio::main]
-async fn main(){
+async fn main() {
     let model = Models::HiddenMarkovModel;
     let output = "src/data/test.csv";
     let params = EmResult {
         initial: 0.3,
         transition: 0.15,
         slip: 0.15,
-        guess: 0.30
-    }; 
+        guess: 0.30,
+    };
 
     let _ = expectation_maximisation(model, params, "src/data/train_data.csv", output).await;
-
-} 
+}
